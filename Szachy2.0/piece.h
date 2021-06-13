be90e9c3e7d4fc3ea29e0454f55b9cc389b9e3a5
Fibern <72557793+Fibern@users.noreply.github.com>
2021-06-13 17:05:14 +0200

    a

--- Szachy2.0/piece.h
+++ Szachy2.0/piece.h
@@ -2,12 +2,7 @@
 #include <SFML/Graphics.hpp>
 #include <iostream>
 #include "pieceTex.h"
-//#include <time.h>
-//#include <math.h>
-//
-//using namespace sf;
-//using namespace std;
-//
+
 class Piece 
 {
 private:
@@ -24,10 +19,8 @@ public:
 
 	Sprite sprite;
 
-//	Piece(int row, int col, bool color, char id, Sprite s);
-//	void Update();
 	Piece() {}
 	void setPiece(char type, bool color, int x, int y);
 	~Piece();
 };
-//
+
